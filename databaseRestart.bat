84b89118bafeee49cf5bafed91a565f0c2064820
Yuliiasergiivna <krempolska@gmail.com>
2025-09-15 10:21:21 +0200

    bat

---
+++ databaseRestart.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Ce script va supprimer la base de données et la recréer.
+symfony console doctrine:database:drop --force
+symfony console doctrine:database:create
+
+del migrations\Ve*
+symfony console make:migration --no-interaction
+symfony console doctrine:migrations:migrate --no-interaction
+symfony console make:fixtures:load --no-interaction
