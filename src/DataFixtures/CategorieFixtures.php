e44bb52b6ee7df9d7517d42b5d5d952acb4de4ff
Yuliiasergiivna <krempolska@gmail.com>
2025-09-15 09:02:28 +0200

    composer

---
+++ src/DataFixtures/CategorieFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Category;
+use Faker\Factory;
+
+class CategorieFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create("fr_BE");
+        for($i = 1; $i <= 5; $i++){
+            $category = new Category();
+            $category->setName($faker->word());
+            $category->setDescription($faker->text(100));
+
+            $manager->persist($category);
+
+        }
+
+        $manager->flush();
+    }
+}
