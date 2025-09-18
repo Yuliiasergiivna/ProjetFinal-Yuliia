f284ecea80d9776b4d227431889c2d39febc9f20
Yuliiasergiivna <krempolska@gmail.com>
2025-09-18 10:49:06 +0200

    login et fixtures

--- migrations/Version20250918084735.php
+++ migrations/Version20250918084735.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250918081256 extends AbstractMigration
+final class Version20250918084735 extends AbstractMigration
 {
     public function getDescription(): string
     {
